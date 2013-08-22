b8590fe61b9f8d6dbee2bc762ec6902c1b82b4f2
Jaran Nilsen <jaran.nilsen@gmail.com>
2013-08-22 14:39:33 +0200

    fixed indentation

--- src/com/scireum/open/xml/XMLReader.java
+++ src/com/scireum/open/xml/XMLReader.java
@@ -53,153 +53,154 @@ import org.xml.sax.helpers.DefaultHandler;
  */
 public class XMLReader extends DefaultHandler {
 
-    private boolean isTextNode = false;
+  private boolean isTextNode = false;
 
-    private StringBuilder textNode = new StringBuilder();
+  private StringBuilder textNode = new StringBuilder();
 
 
-    @Override
-    public void characters(char[] ch, int start, int length)
-            throws SAXException {
-        isTextNode = true;
-        textNode.append(ch, start, length);
+  @Override
+  public void characters(char[] ch, int start, int length)
+          throws SAXException {
+
+    isTextNode = true;
+    textNode.append(ch, start, length);
+  }
+
+
+  @Override
+  public void endDocument() throws SAXException {
+    // Consider iterating over all activeHandler which are not complete
+    // yet and raise an exception.
+    // For now this is simply ignored to make processing more robust.
+  }
+
+
+  @Override
+  public void endElement(String uri, String localName, String name)
+          throws SAXException {
+    // Delegate to active handlers and deletes them if they are finished...
+
+    if (isTextNode) {
+      String data = textNode.toString();
+      for (SAX2DOMHandler handler : activeHandlers) {
+        handler.text(data);
+      }
+      textNode = new StringBuilder();
+      isTextNode = false;
     }
 
-
-    @Override
-    public void endDocument() throws SAXException {
-        // Consider iterating over all activeHandler which are not complete
-        // yet and raise an exception.
-        // For now this is simply ignored to make processing more robust.
+    Iterator<SAX2DOMHandler> iter = activeHandlers.iterator();
+    while (iter.hasNext()) {
+      SAX2DOMHandler handler = iter.next();
+      if (handler.endElement(uri, name)) {
+        iter.remove();
+      }
     }
+  }
 
 
-    @Override
-    public void endElement(String uri, String localName, String name)
-            throws SAXException {
-        // Delegate to active handlers and deletes them if they are finished...
+  @Override
+  public void processingInstruction(String target, String data)
+          throws SAXException {
+    // Delegate to active handlers...
+    for (SAX2DOMHandler handler : activeHandlers) {
+      handler.processingInstruction(target, data);
+    }
+  }
 
-        if (isTextNode) {
-            String data = textNode.toString();
-            for (SAX2DOMHandler handler : activeHandlers) {
-                handler.text(data);
+
+  @Override
+  public void startElement(String uri, String localName, String name,
+                           Attributes attributes) throws SAXException {
+    // Delegate to active handlers...
+    for (SAX2DOMHandler handler : activeHandlers) {
+      handler.startElement(uri, name, attributes);
+    }
+    // Start a new handler is necessary
+    try {
+      // QName qualifiedName = new QName(uri, localName);
+      NodeHandler handler = handlers.get(name);
+      if (handler != null) {
+        activeHandlers.add(new SAX2DOMHandler(handler, uri, name,
+                attributes));
+      }
+    } catch (ParserConfigurationException e) {
+      throw new SAXException(e);
+    }
+  }
+
+
+  private Map<String, NodeHandler> handlers = new TreeMap<String, NodeHandler>();
+
+  private List<SAX2DOMHandler> activeHandlers = new ArrayList<SAX2DOMHandler>();
+
+
+  /**
+   * Registers a new handler for a qualified name of a node. Handlers are
+   * invoked AFTER the complete node was read. Since documents like BMECat
+   * usually don't mix XML-data, namespaces are ignored for now which eases
+   * the processing a lot (especially xpath related tasks). Namespaces however
+   * could be easily added by repalcing String with QName here.
+   */
+
+
+  public void addHandler(String name, NodeHandler handler) {
+
+    handlers.put(name, handler);
+  }
+
+
+  /**
+   * Returns a XMLNode for the given w3c node.
+   */
+  public static StructuredNode convert(Node node) {
+
+    return new XMLNodeImpl(node);
+  }
+
+
+  class UserInterruptException extends RuntimeException {
+
+    private static final long serialVersionUID = -7454219131982518216L;
+  }
+
+
+  /**
+   * Parses the given stream and using the given monitor
+   */
+  public void parse(InputStream stream) throws ParserConfigurationException,
+          SAXException, IOException {
+
+    try {
+      SAXParserFactory factory = SAXParserFactory.newInstance();
+      SAXParser saxParser = factory.newSAXParser();
+      org.xml.sax.XMLReader reader = saxParser.getXMLReader();
+      reader.setEntityResolver(new EntityResolver() {
+
+        public InputSource resolveEntity(String publicId,
+                                         String systemId) throws SAXException, IOException {
+
+          URL url = new URL(systemId);
+          // Check if file is local
+          if ("file".equals(url.getProtocol())) {
+            // Check if file exists
+            File file = new File(url.getFile());
+            if (file.exists()) {
+              return new InputSource(new FileInputStream(file));
             }
-            textNode = new StringBuilder();
-            isTextNode = false;
+          }
+          return null;
         }
-
-        Iterator<SAX2DOMHandler> iter = activeHandlers.iterator();
-        while (iter.hasNext()) {
-            SAX2DOMHandler handler = iter.next();
-            if (handler.endElement(uri, name)) {
-                iter.remove();
-            }
-        }
-    }
-
-
-    @Override
-    public void processingInstruction(String target, String data)
-            throws SAXException {
-        // Delegate to active handlers...
-        for (SAX2DOMHandler handler : activeHandlers) {
-            handler.processingInstruction(target, data);
-        }
-    }
-
-
-    @Override
-    public void startElement(String uri, String localName, String name,
-                             Attributes attributes) throws SAXException {
-        // Delegate to active handlers...
-        for (SAX2DOMHandler handler : activeHandlers) {
-            handler.startElement(uri, name, attributes);
-        }
-        // Start a new handler is necessary
-        try {
-            // QName qualifiedName = new QName(uri, localName);
-            NodeHandler handler = handlers.get(name);
-            if (handler != null) {
-                activeHandlers.add(new SAX2DOMHandler(handler, uri, name,
-                        attributes));
-            }
-        } catch (ParserConfigurationException e) {
-            throw new SAXException(e);
-        }
-    }
-
-
-    private Map<String, NodeHandler> handlers = new TreeMap<String, NodeHandler>();
-
-    private List<SAX2DOMHandler> activeHandlers = new ArrayList<SAX2DOMHandler>();
-
-
-    /**
-     * Registers a new handler for a qualified name of a node. Handlers are
-     * invoked AFTER the complete node was read. Since documents like BMECat
-     * usually don't mix XML-data, namespaces are ignored for now which eases
-     * the processing a lot (especially xpath related tasks). Namespaces however
-     * could be easily added by repalcing String with QName here.
+      });
+      reader.setContentHandler(this);
+      reader.parse(new InputSource(stream));
+    } catch (UserInterruptException e) {
+          /*
+           * IGNORED - this is used to cancel parsing if the used tried to
+     * cancel a process.
      */
-
-
-    public void addHandler(String name, NodeHandler handler) {
-
-        handlers.put(name, handler);
+    } finally {
+      stream.close();
     }
-
-
-    /**
-     * Returns a XMLNode for the given w3c node.
-     */
-    public static StructuredNode convert(Node node) {
-
-        return new XMLNodeImpl(node);
-    }
-
-
-    class UserInterruptException extends RuntimeException {
-
-        private static final long serialVersionUID = -7454219131982518216L;
-    }
-
-
-    /**
-     * Parses the given stream and using the given monitor
-     */
-    public void parse(InputStream stream) throws ParserConfigurationException,
-            SAXException, IOException {
-
-        try {
-            SAXParserFactory factory = SAXParserFactory.newInstance();
-            SAXParser saxParser = factory.newSAXParser();
-            org.xml.sax.XMLReader reader = saxParser.getXMLReader();
-            reader.setEntityResolver(new EntityResolver() {
-
-                public InputSource resolveEntity(String publicId,
-                                                 String systemId) throws SAXException, IOException {
-
-                    URL url = new URL(systemId);
-                    // Check if file is local
-                    if ("file".equals(url.getProtocol())) {
-                        // Check if file exists
-                        File file = new File(url.getFile());
-                        if (file.exists()) {
-                            return new InputSource(new FileInputStream(file));
-                        }
-                    }
-                    return null;
-                }
-            });
-            reader.setContentHandler(this);
-            reader.parse(new InputSource(stream));
-        } catch (UserInterruptException e) {
-            /*
-             * IGNORED - this is used to cancel parsing if the used tried to
-			 * cancel a process.
-			 */
-        } finally {
-            stream.close();
-        }
-    }
+  }
 }
